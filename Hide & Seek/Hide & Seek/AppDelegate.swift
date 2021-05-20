ae83bc8c5315a6defc984113c38a3d486a1b976c
Rahul Kumar <55308841+rahul13199kumar@users.noreply.github.com>
2021-05-20 14:02:09 +0530

    Add files via upload

---
+++ Hide & Seek/Hide & Seek/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hide & Seek
+//
+//  Created by RAHUL KUMAR on 20/05/21.
+//  Copyright © 2021 RAHUL KUMAR. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hide & Seek/Hide & Seek/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hide & Seek
+//
+//  Created by RAHUL KUMAR on 20/05/21.
+//  Copyright © 2021 RAHUL KUMAR. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
